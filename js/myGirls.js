3444273d412990f8bf3db562eb1155b1a0419385
zsualk <zsualk75@gmail.com>
2015-07-23 11:43:05 +0100

    sanyi
    bela

---
+++ js/myGirls.js
@@ -0,0 +1,63 @@
+// JavaScript Document
+myFunction([
+{
+"display": "Jolika",
+"src": "img/temp/masonry/6.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/2.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/4.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/1.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/7.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/4.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/6.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/2.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/4.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/1.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/7.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/4.jpg",
+"url": "services.html"
+},
+]);
---
+++ js/thisGirl.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+myFunction([
+{
+"display": "Jolika",
+"src": "img/temp/masonry/6.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/3.jpg",
+"url": "services.html"
+},
+{
+"display": "Jolika",
+"src": "img/temp/masonry/9.jpg",
+"url": "services.html"
+}
+])
